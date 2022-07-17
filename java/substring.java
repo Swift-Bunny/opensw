346eb00e156317fb1bc15d242fc4d5eec391cf1f
developersung13 <developersung13@gmail.com>
2022-07-17 15:29:56 +0900

    Upload asset

---
+++ java/substring.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+ 
+public class Test_Code {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        String dimension_array = "Hello";
+        System.out.print(dimension_array.substring(2,4));
+    }
+}
